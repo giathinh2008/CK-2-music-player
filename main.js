8e208920469a25c41fa13bcff0df769148cc0b17
giathinh2008 <giathinh3101@gmail.com>
2025-04-09 01:44:01 +0700

    fadfafafsdaf

---
+++ main.js
@@ -0,0 +1,34 @@
+let progress = document.getElementById("progress")
+let song  = document.getElementById("song")
+let playicon = document.getElementById("playicon")
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(playicon.classList.contains("fa-pause")){
+        song.pause();
+        playicon.classList.remove("fa-pause");
+        playicon.classList.add("fa-play");
+    }
+    else {
+        song.play();
+        playicon.classList.add("fa-pause");
+        playicon.classList.remove("fa-play");
+    }
+}
+
+if(song.play){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    playicon.classList.add("fa-pause");
+    playicon.classList.remove("fa-play");
+}
